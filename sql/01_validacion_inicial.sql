93d88c8e6c79252481c221d67686e58a3aa72d1c
MythicalCl <71949833+MythicalCl@users.noreply.github.com>
2026-05-04 21:47:05 -0600

    Validacion inicial del Datawarehouse AdventureWorksDW2019
    Se agrega el script sql en el que se validan las dimensiones y la tabla de hechos del DWH

---
+++ sql/01_validacion_inicial.sql
@@ -0,0 +1,9 @@
+USE AdventureWorksDW2019;
+GO
+
+SELECT TOP 10 * FROM dbo.FactInternetSales;
+SELECT TOP 10 * FROM dbo.DimProduct;
+SELECT TOP 10 * FROM dbo.DimCustomer;
+SELECT TOP 10 * FROM dbo.DimDate;
+SELECT TOP 10 * FROM dbo.DimGeography;
+SELECT TOP 10 * FROM dbo.DimSalesTerritory;
